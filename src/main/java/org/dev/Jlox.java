4c7fc6212a6fd7348e48851752fbfffc5ad771f2
Prabhat Rai <prarai@paloaltonetworks.com>
2022-07-24 16:19:49 +0530

    Add scanner to parse tokens in program

---
+++ src/main/java/org/dev/Jlox.java
@@ -0,0 +1,67 @@
+package org.dev;
+
+import org.dev.data.Token;
+import org.dev.services.JloxScanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Jlox {
+
+    public static boolean hadError = false;
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Unknown number of flags");
+            System.exit(64);
+        } else if (args.length == 1){
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hadError)
+            System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        JloxScanner scanner = new JloxScanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    public static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[Line " + line + " ] Error " + where + ": " + message
+        );
+        hadError = true;
+    }
+}
